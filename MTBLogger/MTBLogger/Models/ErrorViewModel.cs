fe2c72724cb5ec4a137d94d14a8c779e2955f92f
Erik Hanson <ErikHanson135@gmail.com>
2021-01-17 19:58:09 -0800

    Adding initial files

---
+++ MTBLogger/MTBLogger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MTBLogger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
